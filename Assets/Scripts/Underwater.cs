7f66bbd249096d8e5da3026825ebd1b00abd3f07
Jesserockz <jesserockz@me.com>
2013-03-29 23:36:43 +1300

    Fixed water errors

--- Assets/Scripts/Underwater.cs
+++ Assets/Scripts/Underwater.cs
@@ -9,15 +9,19 @@ public class Underwater : MonoBehaviour {
     public int underwaterLevel = 0;
  
     //The scene's default fog settings
-    private bool defaultFog = RenderSettings.fog;
-    private Color defaultFogColor = RenderSettings.fogColor;
-    private float defaultFogDensity = RenderSettings.fogDensity;
-    private Material defaultSkybox = RenderSettings.skybox;
+    private bool defaultFog;
+    private Color defaultFogColor;
+    private float defaultFogDensity;
+    private Material defaultSkybox;
     private Material noSkybox;
  
     void Start () {
 	    //Set the background color
 	    camera.backgroundColor = new Color(0, 0.4f, 0.7f, 1);
+		defaultFog = RenderSettings.fog;
+		defaultFogColor = RenderSettings.fogColor;
+		defaultFogDensity = RenderSettings.fogDensity;
+		defaultSkybox = RenderSettings.skybox;
     }
  
     void Update () {
@@ -25,7 +29,7 @@ public class Underwater : MonoBehaviour {
         {
             RenderSettings.fog = true;
             RenderSettings.fogColor = new Color(0, 0.4f, 0.7f, 0.6f);
-            RenderSettings.fogDensity = 0.04f;
+            RenderSettings.fogDensity = 0.08f;
             //RenderSettings.skybox = noSkybox;
         }
         else
