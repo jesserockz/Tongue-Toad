f6dbda78cf8a59d1f95b368ffec5ea09d4cc1956
Jesserockz <jesserockz@me.com>
2013-03-31 00:15:15 +1300

    Tweaking Camera Still

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 	
-	public float angleToRotate;
+	public float countToRotate;
 	public float timeToRotate;
+	public float rotateAcceleration;
 	
 	bool starting = true;
 	
 	bool rotatemode = false;
-	float curRotation = 0;
+	public float curRotation = 0;
+	public bool slidingOut = false;
+	public bool slidingIn = false;
+	public float slideInStart = 99999f;
 	// Use this for initialization
 	void Start () {
 	
@@ -22,6 +26,8 @@ public class CameraControl : MonoBehaviour {
 			rotatemode = true;
 			curRotation = 0;
 			Player.currentEnergy = 100;
+			slidingOut = true;
+			
 		}
 		
 		Debug.Log(transform.name);
@@ -30,15 +36,29 @@ public class CameraControl : MonoBehaviour {
 	//FixedUpdate is called a fixed number of times per second
 	void FixedUpdate(){
 		if(rotatemode){
-			if(curRotation<angleToRotate/2) timeToRotate -= 0.07f;
-			else if(curRotation>angleToRotate/2) timeToRotate +=0.07f;
-			float rot = Time.deltaTime / timeToRotate * angleToRotate;
+			if(camera.fieldOfView<80 && slidingOut)
+				camera.fieldOfView+=0.1f;
+			else if(camera.fieldOfView>=80 && slidingOut){
+				slidingOut = false;
+				slideInStart = (countToRotate*360)-curRotation;
+			}
+			
+			if(camera.fieldOfView>52 && slidingIn)
+				camera.fieldOfView-=0.1f;
+			else if(camera.fieldOfView<52 && slidingIn)
+				slidingIn = false;
+			
+			if(curRotation>=slideInStart) slidingIn = true;
+			
+			if(curRotation<countToRotate*360/2) timeToRotate -= rotateAcceleration;
+			else if(curRotation>countToRotate*360/2) timeToRotate += rotateAcceleration;
+			float rot = Time.deltaTime / timeToRotate * countToRotate*360;
 			transform.RotateAround (new Vector3(0,2,2), Vector3.left, -rot);
 			curRotation += rot;
 			
-			if (curRotation >= angleToRotate) {
+			if (curRotation >= countToRotate*360) {
 				//ensure the player is back at correct point
-				transform.RotateAround (new Vector3(0,2,2), Vector3.left, (curRotation - angleToRotate));
+				transform.RotateAround (new Vector3(0,2,2), Vector3.left, (curRotation - countToRotate*360));
 				rotatemode = false;
 			}
 		}	
