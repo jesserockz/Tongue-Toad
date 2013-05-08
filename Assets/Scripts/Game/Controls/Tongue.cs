1042994b2c66d4db987262cb5be9195e1e455b75
Jesserockz <jesserockz@me.com>
2013-05-08 21:56:08 +1200

    Fixed animations

--- Assets/Scripts/Game/Controls/Tongue.cs
+++ Assets/Scripts/Game/Controls/Tongue.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 using System.Collections;
 
 public class Tongue : MonoBehaviour
-{	
-	public bool tongueOut = false;
-	public bool tongueRetracting = false;
-	public float tongueNormalSpeed = 1.5f;
+{
+    public bool tongueOut = false;
+    public bool tongueRetracting = false;
+    public float tongueNormalSpeed = 5.0f;//1.5f;
     float tongueSpeed = 0.0f;
-	float retractSpeed = 0.65f;
+    float retractSpeed = 4.0f;//0.65f;
 
     public float maxExtension = 15f;
-	
-	public Transform toad;
-	public Transform tongue;
-	
-	private Player player;
-	private PlayerSounds playerSounds;
-	
-	// Use this for initialization
-	void Start ()
-	{
-		GameObject p = GameObject.FindGameObjectWithTag("Player");
-		player = p.GetComponent<Player>();
-		playerSounds = p.GetComponent<PlayerSounds>();
-		
+
+    public Transform toad;
+    public Transform tongue;
+
+    private Player player;
+    private PlayerSounds playerSounds;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject p = GameObject.FindGameObjectWithTag("Player");
+        player = p.GetComponent<Player>();
+        playerSounds = p.GetComponent<PlayerSounds>();
+
         tongueSpeed = tongueNormalSpeed;
-	}
-	
-	// Update is called once per frame
+    }
+
+    // Update is called once per frame
     void Update()
     {
         //return if paused
@@ -38,12 +38,12 @@ public class Tongue : MonoBehaviour
 
         if (shoot && !tongueOut)
         { //User has started tongue extension
-            //toad.GetComponent<MouseFollower> ().enabled = false;
+            toad.GetComponent<MouseFollower> ().enabled = false;
             transform.rotation = toad.rotation;
             tongue.rotation = toad.rotation;
             tongueOut = true;
-			playerSounds.tongueStartSource.Play();
-			playerSounds.tongueStretchSource.Play ();
+            playerSounds.tongueStartSource.Play();
+            playerSounds.tongueStretchSource.Play();
         }
         else if (shoot && tongueOut && !tongueRetracting)
         { //Tongue extension continuing out
@@ -71,15 +71,15 @@ public class Tongue : MonoBehaviour
         {
             //Tongue retracting back into mouth
             tongueRetracting = true;
-			
-			
-			float mult = (maxExtension  - Vector3.Distance(transform.position, toadPosition())) / maxExtension;
-			//keeps the speed between 0.5-0.9, the tongue getting faster as it approaches
-            tongueSpeed = Mathf.Min (Mathf.Max (0.6f, mult * 1.5f), 1f);
+
+
+            float mult = (maxExtension - Vector3.Distance(transform.position, toadPosition())) / maxExtension;
+            //keeps the speed between 0.5-0.9, the tongue getting faster as it approaches
+            tongueSpeed = Mathf.Min(Mathf.Max(0.6f, mult * 1.5f), 1f);
             //tongueSpeed = 0.8f;
-			transform.LookAt(toadPosition());
+            transform.LookAt(toadPosition());
             transform.position += transform.forward * tongueSpeed;
-            
+
             float newX = ((toad.position.x - transform.position.x) / 2) + transform.position.x;
             float newZ = ((toad.position.z - transform.position.z) / 2) + transform.position.z;
             Vector3 newTonguePos = new Vector3(newX, toadPosition().y, newZ);
@@ -88,8 +88,8 @@ public class Tongue : MonoBehaviour
             tongue.position = newTonguePos;
             tongue.localScale = new Vector3(0.1f, 0.1f, distance);
             tongue.LookAt(transform);
-			
-			//if the distance is less tha
+
+            //if the distance is less tha
             if (transform.position.z < toadPosition().z || Vector3.Distance(transform.position, toadPosition()) <= 0.2)
             {
                 tongueRetracting = false;
@@ -104,44 +104,47 @@ public class Tongue : MonoBehaviour
         }
         else if (!tongueOut && !shoot)
         {
-						playerSounds.tongueStretchSource.Stop ();
+            playerSounds.tongueStretchSource.Stop();
 
             //Tongue dormant inside mouth waiting to ATTACK!!!
             tongueRetracting = false;
-            //toad.GetComponent<MouseFollower> ().enabled = true;
-            //transform.position = toadPosition();
+
+            toad.GetComponent<MouseFollower>().enabled = true;
+            transform.position = toadPosition();
+            tongue.position = toadPosition();
+
             tongue.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-            //tongue.position = toadPosition();
+
             tongueSpeed = tongueNormalSpeed;
         }
     }
 
-	Vector3 toadPosition ()
-	{
-		return toad.position + new Vector3 (0f, 0.50f, 0.35f);
-	}
-	
-	void OnTriggerEnter (Collider other)
-	{
-		GameObject o = other.gameObject;
+    Vector3 toadPosition()
+    {
+        return toad.position + new Vector3(0f, 0.50f, 0.35f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject o = other.gameObject;
         //Debug.Log("TongueTip collided with " + o.tag);
         if (o.tag == "Enemy")
         {
             Debug.Log("TongueTip collided with " + o.tag);
 
-			//get the enemy
-			Enemy enemy = o.GetComponent<Enemy>();
-			
-			//direction of impact
-			Vector3 dir = Vector3.Normalize(-(toadPosition() - transform.position));
-			
-			//fling the enemy back
-			o.GetComponent<Rigidbody>().velocity = (3.0f * dir);
-			
-			//attack the enemy... death, animations, etc, are handled there.
-			enemy.attack(player.getTongueDamage());
-			
-			//now tell the player they've attacked an enemy. That stuff is handled there
+            //get the enemy
+            Enemy enemy = o.GetComponent<Enemy>();
+
+            //direction of impact
+            Vector3 dir = Vector3.Normalize(-(toadPosition() - transform.position));
+
+            //fling the enemy back
+            o.GetComponent<Rigidbody>().velocity = (3.0f * dir);
+
+            //attack the enemy... death, animations, etc, are handled there.
+            enemy.attack(player.getTongueDamage());
+
+            //now tell the player they've attacked an enemy. That stuff is handled there
             player.attackEnemy(enemy);
         }
         else if (o.tag == "Friendly")
@@ -155,7 +158,7 @@ public class Tongue : MonoBehaviour
         {
             Debug.Log("TongueTip collided with " + o.tag);
 
-			tongueRetracting = true;
-		}
-	}
+            tongueRetracting = true;
+        }
+    }
 }
