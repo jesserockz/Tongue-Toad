8b678cc6322ba56137fa6712250b01ca51f34b44
Troy Shaw <troyshw@gmail.com>
2013-06-02 23:20:05 +1200

    Added wave system. Doesn't display info panels yet, but monsters spawn. Also added the panels to resources. Also added windows shader for when working on windows machine.

--- Assets/Scripts/Game/Enemies/EnemySpawn.cs
+++ Assets/Scripts/Game/Enemies/EnemySpawn.cs
@@ -1,209 +1,245 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class EnemySpawn : MonoBehaviour {
+public class EnemySpawn : MonoBehaviour
+{
 	//public GameObject enemy;
-    public GameObject friendly;
-    public GameObject craftCarrier;
-    //public GameObject snail;
-	public GameObject[] plebSnail;
-    public GameObject[] copterSnail;
-    public GameObject[] planeSnail;
-    public GameObject[] tetsudoSnail;
+	public GameObject friendly;
+	public GameObject craftCarrier;
 	
+	//public GameObject snail;
+	public GameObject[] plebSnail;
+	public GameObject[] copterSnail;
+	public GameObject[] planeSnail;
+	public GameObject[] tetsudoSnail;
 	public GameObject[] lineSpawnPoint;
+	
+	private List<List<System.Action>> formations;
+	private List<System.Action> currentFormation;
 	
 	private GameObject currentBoss;
 	
+	private int currentWave;
 	private float between = 2.0f;
 	private float lastSpawn = 0;
-    private bool boss = false;
-
-    private int spawnCount = 20;
+	private bool boss = false;
 	
-	private Quaternion spawnAngle = Quaternion.Euler(0, 180, 0);
+	private int spawnCount = 20;
+	private Quaternion spawnAngle = Quaternion.Euler (0, 180, 0);
 	
-	void Start() {
+	void Start ()
+	{
+		// Create the waves
 		
+		currentFormation = new List<System.Action> ();
+		
+		formations = new List<List<System.Action>> ();
+		
+		for (int i = 0; i < 10; i++) {
+			formations.Add (new List<System.Action> ());
+		}
+		
+		//wave 1 – 10 plebs
+		addObject (formations [0], 10, spawnPleb);
+		
+		//wave 2 - 8 plebs, 5 testudo
+		addObject (formations [1], 8, spawnPleb);
+		addObject (formations [1], 5, spawnTetsudo);
+		
+		//wave 3 - 10 plebs, 4 toads
+		addObject (formations [2], 10, spawnPleb);
+		addObject (formations [2], 4, spawnGood);
+		
+		//wave 4 - 8 plebs, 5 helicopter, 3 toads
+		addObject (formations [3], 8, spawnPleb);
+		addObject (formations [3], 5, spawnCopter);
+		addObject (formations [3], 3, spawnGood);
+		
+		//wave 5 - 8 plebs, 2 testudo, 2 helicopter, 3 toads
+		addObject (formations [4], 8, spawnPleb);
+		addObject (formations [4], 2, spawnTetsudo);
+		addObject (formations [4], 2, spawnCopter);
+		addObject (formations [4], 3, spawnGood);
+		
+		//wave 6 - 10 plebs, 5 planes, 3 toads
+		addObject (formations [5], 10, spawnPleb);
+		addObject (formations [5], 5, spawnPlane);
+		addObject (formations [5], 3, spawnGood);
+		
+		//wave 7 - 10 plebs, 2 testudo, 3 helicopter, 2 planes, 4 toads
+		addObject (formations [6], 10, spawnPleb);
+		addObject (formations [6], 2, spawnTetsudo);
+		addObject (formations [6], 3, spawnCopter);
+		addObject (formations [6], 2, spawnPlane);
+		addObject (formations [6], 4, spawnGood);
+		
+		//wave 8 - 8 plebs, 10 testudo, 5 toads
+		addObject (formations [7], 8, spawnPleb);
+		addObject (formations [7], 10, spawnTetsudo);
+		addObject (formations [7], 5, spawnGood);
+		
+		//wave 9 - 10 plebs, 4 helicopter, 4 planes, 4 testudo, 4 toads
+		addObject (formations [8], 10, spawnPleb);
+		addObject (formations [8], 4, spawnCopter);
+		addObject (formations [8], 4, spawnPlane);
+		addObject (formations [8], 4, spawnTetsudo);
+		addObject (formations [8], 4, spawnGood);
+		
+		//wave 10 - SnailCraftCarrier (15 planes)
+		addObject (formations [9], 1, spawnCraftCarrier);
+		
+		//set our current formation to the first formation
+		currentFormation = formations [0];
+	}
+	
+	private void addObject (List<System.Action> addTo, int toAdd, System.Action method)
+	{
+		for (int i = 0; i < toAdd; i++) {
+			addTo.Add (method);
+		}
 	}
 	
 	// Update is called once per frame
-    void Update()
-    {
-        if (boss) return;
-        if (Time.time - lastSpawn > between)
-        {
-            //Instantiate(enemyPrefab);
-
-            int val = Random.Range(0, 110);
-
-
-            //get a new between value
-            between = Random.Range(0.1f, 2.0f);
-
-            if (spawnCount == 0)
-            {
-                spawnCraftCarrier();
-                boss = true;
-                return;
-            }
-
-
-            if (val < 20) spawnGood();
-            else if (val < 70) spawnPleb();
-            else if (val < 90) spawnCopter();
-            else if (val < 100) spawnPlane();
-            else spawnTetsudo();
-
-            spawnCount--;
-
-            lastSpawn = Time.time;
-        }
-    }
+	void Update ()
+	{
+		if (true && !boss) {
+			//not enough time has elapsed to spawn a new monster. Return
+			if (Time.time - lastSpawn <= between)
+				return;
+			
+			//get a random spawn method from list...
+			int i = Random.Range (0, currentFormation.Count);
+			System.Action action = currentFormation [i];
+			
+			//spawn them, then delete it from list
+			action.Invoke ();
+			
+			currentFormation.RemoveAt (i);
+			
+			//if we've exhausted that list, call to populate new wave
+			if (currentFormation.Count == 0)
+				populateNextSpawnWave ();
+			
+			//set last spawn time
+			lastSpawn = Time.time;
+			
+			return;
+		}
+	}
 	
-	public void resumeSpawning(){
+	public void resumeSpawning ()
+	{
 		boss = false;
 		Destroy (currentBoss);
 		spawnCount = 20;
 	}
-
-    private void spawnCraftCarrier()
-    {
-        Vector3 pos = new Vector3(1f, -1f, 41f);
-        currentBoss =  (GameObject)Instantiate(craftCarrier, pos, Quaternion.Euler(0, 180, 0));
-    }
 	
-	private void spawnGood()
+	private void populateNextSpawnWave ()
+	{
+		currentWave++;
+		
+		if (currentWave < formations.Count) {
+			//spawning from predetermined lists...
+				
+			currentFormation = formations [currentWave];
+				
+		} else {
+			if (currentWave + 1 % 5 == 0) {
+				//spawn boss
+				addObject (currentFormation, 1, spawnCraftCarrier);
+			} else {
+				//spawning based on pattern
+				//y = x - 9	(zero based index)
+				//Wave x – (6 + y) plebs, (1 + y) helicopter, (2 + y) testudo, (2 + y) planes
+				int y = currentWave - 9;
+			
+				addObject (currentFormation, 6 + y, spawnPleb);
+				addObject (currentFormation, 1 + y, spawnCopter);
+				addObject (currentFormation, 2 + y, spawnTetsudo);
+				addObject (currentFormation, 2 + y, spawnPlane);
+			}
+		}
+		
+		Debug.Log ("wave:" + currentWave);
+	}
+	
+	private void spawnCraftCarrier ()
+	{
+		boss = true;
+		Vector3 pos = new Vector3 (1f, -1f, 41f);
+		currentBoss = (GameObject)Instantiate (craftCarrier, pos, Quaternion.Euler (0, 180, 0));
+	}
+	
+	private void spawnGood ()
 	{ 
-		Vector3 middle = getLinePosition();
+		Vector3 middle = getLinePosition ();
 		middle.y = -0.35f;
 		
-		GameObject o = (GameObject) Instantiate(friendly, middle, Quaternion.Euler(0, -180, 0));
+		GameObject o = (GameObject)Instantiate (friendly, middle, Quaternion.Euler (0, -180, 0));
 		//quick hack to make it loop animation
-		o.GetComponent<Animation>()["friendly toad animation"].wrapMode = WrapMode.Loop;
+		o.GetComponent<Animation> () ["friendly toad animation"].wrapMode = WrapMode.Loop;
 	}
 	
-	
-	private void spawnPleb()
+	private void spawnPleb ()
 	{
-		GameObject snail = getRandomPleb();
-		Vector3 point = getLinePosition();
+		GameObject snail = getRandomPleb ();
+		Vector3 point = getLinePosition ();
 		
-		Instantiate(snail, point, Quaternion.Euler(0, -180, 0));
+		Instantiate (snail, point, Quaternion.Euler (0, -180, 0));
 	}
 	
-	private void spawnCopter()
+	private void spawnCopter ()
 	{
-		GameObject copter = getRandomCopter();
-		Vector3 point = getLinePosition();
+		GameObject copter = getRandomCopter ();
+		Vector3 point = getLinePosition ();
 		
-		Instantiate(copter, point, Quaternion.Euler(0, -180, 0));
+		Instantiate (copter, point, Quaternion.Euler (0, -180, 0));
 		
 	}
 
-    private void spawnPlane()
-    {
-        GameObject plane = getRandomPlane();
-        Vector3 point = getLinePosition();
-        point.y = 0.6f;
+	private void spawnPlane ()
+	{
+		GameObject plane = getRandomPlane ();
+		Vector3 point = getLinePosition ();
+		point.y = 0.6f;
 
-        Instantiate(plane, point, Quaternion.Euler(0, -180, 0));
+		Instantiate (plane, point, Quaternion.Euler (0, -180, 0));
 
-    }
-
-    private void spawnTetsudo()
-    {
-        GameObject tetsudo = getRandomTetsudo();
-        Vector3 point = getLinePosition();
-        point.y = 0.03f;
-
-        Instantiate(tetsudo, point, Quaternion.Euler(0, -180, 0));
-
-    }
-
-	//spawns a few monsters in an abreast formation
-	private void spawnAbreast() 
-	{
-		Vector3 middle = this.transform.position;
-		Vector3 offset = new Vector3(1.5f, 0, 0);
-		GameObject snail = getRandomPleb();
-		
-		Instantiate(snail, middle, Quaternion.Euler(0, -180, 0));
-		Instantiate(snail, middle - offset, Quaternion.Euler(0, -180, 0));
-		Instantiate(snail, middle + offset, Quaternion.Euler(0, -180, 0));
-	}
-	
-	//spawns a few monsters in a line formation
-	private void spawnLine() 
-	{
-		Vector3 middle = getLinePosition();
-		Vector3 offset = new Vector3(0, 0, 5);
-		
-		GameObject snail = getRandomPleb();
-		
-		Instantiate(snail, middle - offset, Quaternion.Euler(0, -180, 0));
-		Instantiate(snail, middle, Quaternion.Euler(0, -180, 0));
-		Instantiate(snail, middle + offset, Quaternion.Euler(0, -180, 0));
-	}
-	
-	//spawns a few monsters in a star formation
-	private void spawnStar() 
-	{
-		
-	}
-	
-	//spawns many little monsters who scatter around
-	private void spawnScatter()
-	{
-		int range = Random.Range (5, 15);
-		for (int i = 0; i < range; i++)
-		{
-            GameObject snail = getRandomPleb();
-			//float xOffset = Random.Range(0.0f, 5.0f);
-			Instantiate(snail, transform.position, Quaternion.Euler(0, -180, 0));
-		}
-	}
-	
-	private void spawnDiagonal()
-	{
-		GameObject snail = getRandomPleb();
-		
-		//multipler will make the diagonal go from left to right, or right to left
-		//with either be -1, 1, 
-		int ran = Random.Range(0, 2);	//0 inclusive to 2 exclusive
-		int orientation = (ran == 0) ? 1 : -1;
-		
-		for (int i = 0; i < lineSpawnPoint.Length; i++)
-		{
-			Instantiate(snail, lineSpawnPoint[i].transform.position + new Vector3(0, 0, orientation * (3 + i * -3)), Quaternion.Euler(0, -180, 0));
-		}
-		
-	}
-	
-	
-	private GameObject getRandomPleb()
-	{
-        return plebSnail[Random.Range(0, plebSnail.Length)];
-	}
-	
-	private GameObject getRandomCopter()
-	{
-        return copterSnail[Random.Range(0, copterSnail.Length)];
 	}
 
-    private GameObject getRandomPlane()
-    {
-        return planeSnail[Random.Range(0, planeSnail.Length)];
-    }
+	private void spawnTetsudo ()
+	{
+		GameObject tetsudo = getRandomTetsudo ();
+		Vector3 point = getLinePosition ();
+		point.y = 0.03f;
 
-    private GameObject getRandomTetsudo()
-    {
-        return tetsudoSnail[Random.Range(0, tetsudoSnail.Length)];
-    }
+		Instantiate (tetsudo, point, Quaternion.Euler (0, -180, 0));
+
+	}
+	
+	private GameObject getRandomPleb ()
+	{
+		return plebSnail [Random.Range (0, plebSnail.Length)];
+	}
+	
+	private GameObject getRandomCopter ()
+	{
+		return copterSnail [Random.Range (0, copterSnail.Length)];
+	}
+
+	private GameObject getRandomPlane ()
+	{
+		return planeSnail [Random.Range (0, planeSnail.Length)];
+	}
+
+	private GameObject getRandomTetsudo ()
+	{
+		return tetsudoSnail [Random.Range (0, tetsudoSnail.Length)];
+	}
 	
 	//returns a random vector of one of the positions of a line formation spawn point 
-	private Vector3 getLinePosition()
+	private Vector3 getLinePosition ()
 	{
-		return lineSpawnPoint[Random.Range (0, lineSpawnPoint.Length)].transform.position;
+		return lineSpawnPoint [Random.Range (0, lineSpawnPoint.Length)].transform.position;
 	}
 }
